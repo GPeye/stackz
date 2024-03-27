f0ae6e03abd93dded5d8155becfd4ba4bb3e228b
GPeye <goldenpyramid@protonmail.com>
2024-03-27 03:43:55 +0000

    working

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 
 #include "pd_api.h"
 #include "lcdpatterns.h"
+#include "mini3d.h"
+#include "3dmath.h"
+#include "shape.h"
+#include "scene.h"
+#include "3dhelper.h"
 
 static int update(void* userdata);
 const char* fontpath = "num";
@@ -11,6 +16,31 @@ PlaydateAPI *pd;
 
 SoundChannel* sound_effects = NULL;
 PDSynth* projectile_sound = NULL;
+Scene3D* scene;
+Shape3D* shape;
+Shape3D* stackbox1;
+Shape3D* activebox1;
+
+Scene3DNode* n;
+Scene3DNode* rootNode;
+Scene3DNode* stackNode;
+Scene3DNode* activeNode;
+
+Point3D* p1;Point3D* p2;Point3D* p3;Point3D* p4;
+Scene3DNode* node1;
+Scene3DNode* node2;
+Matrix3D* rot;
+Matrix3D* rotn;
+
+Scene3DNode* cubenodestop[10];
+Scene3DNode* cubenodestop2[10];
+Scene3DNode* cubenodes[10];
+Scene3DNode* cubenodes2[10];
+Scene3DNode* cubenodesbottom[10];
+Scene3DNode* cubenodesbottom2[10];
+float r = 0.f;
+float ellapsed=0.f;
+float res=0.f;
 
 #ifdef _WINDLL
 __declspec(dllexport)
@@ -43,6 +73,60 @@ int eventHandler(PlaydateAPI* playdate, PDSystemEvent event, uint32_t arg)
 		pd->sound->synth->setSustainLevel(projectile_sound, 1.f);
 		pd->sound->synth->setReleaseTime(projectile_sound, 0.1f);
 		pd->sound->synth->setVolume(projectile_sound, 1.f, 1.f);
+
+		mini3d_setRealloc(pd->system->realloc);
+
+		scene = scene_new();
+		scene_setCameraOrigin(scene, 0.f, 5.f, 6.f);
+		scene_setLight(scene, 0.2f, 0.8f, 0.4f);
+
+		rootNode = Scene3D_getRootNode(scene);
+		stackNode = Scene3DNode_newChild(rootNode);
+		activeNode = Scene3DNode_newChild(rootNode);
+
+		stackbox1 = shape_new_cuboid(1,0.2,1,-0.8f);
+
+		//Vector3D v = { 0,-0.38,0};
+		//Scene3DNode_addShapeWithOffset(stackNode, stackbox1, v);
+		Scene3DNode_addShape(stackNode, stackbox1);
+		Matrix3D tm = matrix_addTranslation(0,-0.38,0);
+		Scene3DNode_addTransform(stackNode, &tm);
+
+
+		activebox1 = shape_new_cuboid(1,0.2,1,-0.3f);
+		Scene3DNode_addShape(activeNode, activebox1);
+
+		//Matrix3D tm2 = matrix_addTranslation(0.f,-1.f,0.f);
+		//Scene3DNode_addTransform(rootNode, &tm2);
+
+		//scene_setCameraOrigin(scene, 0.f, 0.f, 6.f);
+		
+		rot = matrix_newRotation(1.f,0.f,1.f,0.0f);
+		
+
+
+
+		// scene = scene_new();
+		// scene_setCameraOrigin(scene, 0.f, 0.f, 6.f);
+		// scene_setLight(scene, 0.2f, 0.8f, 0.4f);
+
+		// n = Scene3D_getRootNode(scene);
+
+		// Matrix3D tm = matrix_addTranslation(0,0,0);
+		// rotn = matrix_newRotation(60.f,1.f,1.1f,.5f);
+		// Scene3DNode_setTransform(n, rotn);
+		
+
+		// shape = shape_new_cuboid(1,0.2,1,-0.3f);
+		// node1 = Scene3DNode_newChild(n);
+		// node2 = Scene3DNode_newChild(node1);
+		// //Matrix3D tm = matrix_addTranslation(-5,0,0);
+		// //Scene3DNode_addTransform(node1, &tm);
+		// Scene3DNode_addShape(node2, shape);
+
+		// rot = matrix_newRotation(1.5f,1.f,1.f,0.3f);
+		
+		
 	}
 	
 	return 0;
@@ -67,12 +151,54 @@ void DrawBackground(PlaydateAPI *pd)
     pd->graphics->fillEllipse(20, 0, 360, 240, 0.0, 0.0, kColorWhite);
 }
 
+float angle;
+float rotty = 0.f;
+Matrix3D tmm;
 static int update(void* userdata)
 {
 	PlaydateAPI* pd = userdata;
 	DrawBackground(pd);
 
+	if(ellapsed == 0.0f)
+	{
+		pd->system->resetElapsedTime();
+		// pd->graphics->setFont(font);
+		// pd->graphics->drawText("69", strlen("69"), kASCIIEncoding, x, y);
+		pd->sound->synth->playNote(projectile_sound, 700.f, 1.f, 0.1f, 0);
+	}else{
+	}
+	ellapsed = pd->system->getElapsedTime();
 	
+	res = sinf(ellapsed)*5;
+
+	tmm = matrix_addTranslation(res,0,0);
+	Scene3DNode_setTransform(activeNode, &tmm);
+
+	//activebox1->points->x = res;
+
+	//activeNode->transform.dz = res;
+
+	angle = pd->system->getCrankChange();
+
+	Scene3DNode_addTransform(rootNode, matrix_newRotation(angle,0.f,0.f,1.f));
+	Scene3DNode_addTransform(rootNode, rot);
+
+	//Scene3DNode_addTransform(stackNode, rot);
+	
+	
+	rotty +=0.1f;
+	// Matrix3D tm = matrix_addTranslation(res,0,0);
+	// Scene3DNode_setTransform(node2, &tm);
+	
+	// Scene3DNode_addTransform(n, rot);
+	
+	//pd->system->logToConsole("%f\r",node1->transform.dx);
+	
+
+	Scene3D_draw(scene, pd->graphics->getFrame(), LCD_ROWSIZE);
+	pd->graphics->markUpdatedRows(0, LCD_ROWS-1);
+
+	pd->system->drawFPS(0,0);
 
 	return 1;
 }
---
+++ src/mini3d/3dhelper.h
@@ -0,0 +1,34 @@
+// Helper functions similar to the LUA Glue
+
+#ifndef _3D_HELPER_H
+#define _3D_HELPER_H
+
+#include <math.h>
+#include "mini3d.h"
+#include "3dmath.h"
+#include "shape.h"
+#include "scene.h"
+
+Scene3D *scene_new();
+
+void scene_setCameraOrigin(Scene3D *scene, float x, float y, float z);
+
+void scene_setLight(Scene3D *scene, float x, float y, float z);
+
+Shape3D *shape_new();
+
+Shape3D *shape_new_cuboid(float width, float height, float depth, float colorBias);
+
+Point3D *point_new(float x, float y, float z);
+
+Shape3D *shape_addFace(Shape3D *shape, Point3D *a, Point3D *b, Point3D *c, Point3D *d, float colorBias);
+
+Matrix3D *matrix_new();
+
+Matrix3D *matrix_newRotation(float angle, float x, float y, float z);
+
+Matrix3D matrix_addTranslation(float x, float y, float z);
+
+void node_addTransform(Matrix3D *matrix);
+
+#endif
